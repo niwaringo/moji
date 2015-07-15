4494bb4cd57129a88ade6de32d5072d322f261ad
niwaringo:mac <niwaringo@gmail.com>
2015-07-15 23:26:07 +0900

    string拡張の準備

---
+++ src/moji.string.js
@@ -0,0 +1,17 @@
+
+
+//match
+//encode
+//decode
+//replace
+// slice
+//substr
+//toLocaleLowerCase
+//toLocaleUpperCase
+//toLowerCase
+//toUpperCase
+//trim
+//trimLeft
+//trimRight
+//encodeURIComponent
+//decodeURIComponent
