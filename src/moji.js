a165527efc70ad98985d8e0a25b27e21981a8ad3
niwaringo:mac <niwaringo@gmail.com>
2015-07-16 22:50:31 +0900

    mojiStrを継承する

--- src/moji.js
+++ src/moji.js
@@ -1,9 +1,11 @@
 "use strict";
 var Moji = require("./moji.core");
+var mojiStr = require("./moji.string");
 var _mojisyu = require("./default_mojisyu");
 var assign = require("object-assign");
 var mojisyu = assign({}, _mojisyu);
 
+mojiStr.call(Moji.prototype);
 
 var moji = function (str) {
     return new Moji(mojisyu, str);
