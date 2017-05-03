41369baf8216bf08c933d9bd2ce77d7fbc5a1e2b
niwaringo <niwaringo@gmail.com>
2017-05-03 11:41:12 +0900

    不要なファイルを削除

--- src/pairmojisyu.js
+++
@@ -1,8 +0,0 @@
-const Mojisyu = require("./mojisyu");
-
-module.exports = class PairMojisyu {
-    constructor(mojisyus) {
-        this.from = new Mojisyu(mojisyus[0][0], mojisyus[0][1]);
-        this.to = new Mojisyu(mojisyus[1][0], mojisyus[1][1]);
-    }
-};
